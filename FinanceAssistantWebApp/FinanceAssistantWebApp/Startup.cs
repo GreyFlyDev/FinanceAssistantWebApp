4dd5d1c1b4acafc156f476ba508810ce2bbd61fe
GreyFlyDev <gregnnylf94@gmail.com>
2016-07-05 10:17:52 -0400

    Initial

---
+++ FinanceAssistantWebApp/FinanceAssistantWebApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FinanceAssistantWebApp.Startup))]
+namespace FinanceAssistantWebApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
