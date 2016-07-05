4e488a6a4a6f5503d60ba8f73681d3234a8890b3
GreyFlyDev <gregnnylf94@gmail.com>
2016-07-05 10:26:29 -0400

    Added Bill Entity

---
+++ FinanceAssistantWebApp/FinanceAssistantWebApp/Models/Bill.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FinanceAssistantWebApp.Models
+{
+    public class Bill
+    {
+        public int BillId { get; set; }
+        public string BillTitle { get; set; }
+        public DateTime DueDate { get; set; }
+        public decimal Amount { get; set; }
+        public string PayLink { get; set; }
+        public bool Paid { get; set; }
+
+        public string UserId { get; set; }
+    }
+}
